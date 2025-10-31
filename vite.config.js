b14be45891bc8d831f7fe864a2accfd88f0f3950
sml <smendeslopes@gmail.com>
2025-10-31 23:48:52 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/responsabilidade-social/', // igual ao nome do repositório
+  base: '/', // user site -> raiz do domínio
 })
